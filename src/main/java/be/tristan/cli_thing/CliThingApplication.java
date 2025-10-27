1a81ec2f704d8b4a4e60a049be5d00df6ff6e362
tristanvong <146710405+tristanvong@users.noreply.github.com>
2025-10-27 20:50:24 +0100

    Added diff command

--- src/main/java/be/tristan/cli_thing/CliThingApplication.java
+++ src/main/java/be/tristan/cli_thing/CliThingApplication.java
@@ -1,9 +1,12 @@
 package be.tristan.cli_thing;
 
+import be.tristan.cli_thing.commands.tools.DiffChecker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.shell.command.annotation.EnableCommand;
 
 @SpringBootApplication
+@EnableCommand(DiffChecker.class)
 public class CliThingApplication {
 
 	public static void main(String[] args) {
