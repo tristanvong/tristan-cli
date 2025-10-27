0d6df52c67fe4a81eaa8ef88d2c8f56b82ce438d
tristanvong <146710405+tristanvong@users.noreply.github.com>
2025-10-27 21:18:53 +0100

    Added diceware command (early version)
    Would be better to print out (1) the corresponding word(s) (TitleCase),
    (2) maybe not print out and just give the randomized numbers and let the
    user do it manually (probably with a boolean trigger)

--- src/main/java/be/tristan/cli_thing/CliThingApplication.java
+++ src/main/java/be/tristan/cli_thing/CliThingApplication.java
@@ -2,7 +2,9 @@ package be.tristan.cli_thing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.shell.command.annotation.CommandScan;
 
+@CommandScan
 @SpringBootApplication
 public class CliThingApplication {
 
