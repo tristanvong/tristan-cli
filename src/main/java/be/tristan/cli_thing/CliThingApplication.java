0bb168a60f1c823b3c176f70549aac208928dbcc
tristanvong <146710405+tristanvong@users.noreply.github.com>
2025-10-27 20:10:26 +0100

    initialize project
    Files generated by start.spring.io

---
+++ src/main/java/be/tristan/cli_thing/CliThingApplication.java
@@ -0,0 +1,13 @@
+package be.tristan.cli_thing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliThingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CliThingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/tristan/cli_thing/CliThingApplicationTests.java
@@ -0,0 +1,13 @@
+package be.tristan.cli_thing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliThingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
